39e0772836cbc6b9239f06108a3b14c42aa093fd
Kamil Jagoda <kamiljagoda2@gmail.com>
2018-05-14 20:39:39 +0200

    Grunt Server for php files

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,15 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
-        watch: {
-            sass: {
-                files: ['src/styles/*.scss', 'src/styles/*/*.scss', 'build/styles/*.css'],
-                tasks: ['sass', 'cssmin']
-            },
-            concat: {
-                files: ['src/scripts/*.js'],
-                tasks: ['concat', 'uglify']
-            },
-
-            uglify: {
-                files: ['build/scripts/*.js', 'src/scripts/vendors/*.js'],
-                tasks: 'uglify'
+        php: {
+            dist: {
+                options: {
+                    hostname: 'localhost',
+                    port: 3000,
+                    base: 'src',
+                    keepalive: false,
+                    open: false
+                }
             }
         },
 
@@ -58,13 +53,13 @@ module.exports = function (grunt) {
                     dest: 'build/scripts/min/',
                     ext: '.min.js'
                 },
-                {
-                    expand: true,
-                    cwd: 'src/scripts/vendors/',
-                    src: '*.js',
-                    dest: 'build/scripts/min/',
-                    ext: '.min.js'
-                }
+                    {
+                        expand: true,
+                        cwd: 'src/scripts/vendors/',
+                        src: '*.js',
+                        dest: 'build/scripts/min/',
+                        ext: '.min.js'
+                    }
                 ]
             }
         },
@@ -87,16 +82,37 @@ module.exports = function (grunt) {
                     }
                 ]
             }
+        },
+
+        watch: {
+            options: {
+                spawn: false
+            },
+            sass: {
+                files: ['src/styles/*.scss', 'src/styles/*/*.scss', 'build/styles/*.css'],
+                tasks: ['sass', 'cssmin']
+            },
+            concat: {
+                files: ['src/scripts/*.js'],
+                tasks: ['concat', 'uglify']
+            },
+
+            uglify: {
+                files: ['build/scripts/*.js', 'src/scripts/vendors/*.js'],
+                tasks: 'uglify'
+            }
         }
 
     });
 
+    grunt.loadNpmTasks('grunt-php');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.registerTask('default', ['imagemin', 'sass', 'concat', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['imagemin', 'sass', 'concat', 'uglify', 'cssmin','watch']);
+    grunt.registerTask('serve', ['php:dist', 'default']);
 
 };
